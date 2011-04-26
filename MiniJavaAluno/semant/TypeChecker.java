dbbf02947d7cf041f58d763e8d57715573dab529
Anderson Birocchi <anderson.birocchi@gmail.com>
2011-04-26 18:34:37 -0300

    Compilador Minijava Semi-Completo adicionado

---
+++ MiniJavaAluno/semant/TypeChecker.java
@@ -0,0 +1,17 @@
+package semant;
+
+import syntaxtree.Program;
+import errors.ErrorEchoer;
+
+public class TypeChecker
+{
+    private TypeChecker()
+    {
+        super();
+    }
+
+    public static Env TypeCheck(ErrorEchoer err, Program p)
+    {		
+        return null;
+    }
+}
