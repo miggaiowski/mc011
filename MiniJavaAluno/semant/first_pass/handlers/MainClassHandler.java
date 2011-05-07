baecd36196746a33f27fd6eeca921e2473518275
Anderson Birocchi <anderson.birocchi@gmail.com>
2011-05-06 23:52:30 -0300

    Finalmente consegui adicionar os dados da MainClass na tabela de simbolos

--- MiniJavaAluno/semant/first_pass/handlers/MainClassHandler.java
+++ MiniJavaAluno/semant/first_pass/handlers/MainClassHandler.java
@@ -1,10 +1,21 @@
 package semant.first_pass.handlers;
 
+
 import semant.Env;
 import symbol.ClassInfo;
 import symbol.Symbol;
+import syntaxtree.Exp;
+import syntaxtree.Formal;
+import syntaxtree.Identifier;
+import syntaxtree.IdentifierType;
 import syntaxtree.MainClass;
+import syntaxtree.MethodDecl;
+import syntaxtree.Statement;
+import syntaxtree.Type;
+import syntaxtree.VarDecl;
 import syntaxtree.VisitorAdapter;
+import util.List;
+
 
 public class MainClassHandler extends VisitorAdapter {
 
@@ -24,7 +35,13 @@ public class MainClassHandler extends VisitorAdapter {
     	Symbol name = Symbol.symbol(node.className.s);
     	ClassInfo info = new ClassInfo(name);
     	
-    	// TODO Fazer aqui o firstPass do método main
+    	Formal mainArg = new Formal(node.line, node.row, (Type) new IdentifierType(node.line, node.row, "String [] "), node.mainArgName);
+    	List<Formal> fl = new List<Formal>(mainArg,null);
+    	List<Statement> sl = new List<Statement>(node.s,null);
+    	
+    	MethodDecl md = new MethodDecl(node.line, node.row,(Type) new IdentifierType(node.line, node.row, "void "), new Identifier(node.line, node.row, "main "), fl, (List<VarDecl>) null , sl, (Exp)null);
+    	
+    	MethodDeclHandler.firstPass(env, info, md);
     	
     	
     	if (!env.classes.put(name, info)) {
