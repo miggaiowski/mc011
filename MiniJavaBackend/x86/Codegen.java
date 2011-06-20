240d2ee61abdcf39aa9f37606339b16bf579c52c
Anderson Birocchi <anderson.birocchi@gmail.com>
2011-06-20 19:40:08 -0300

    Começando alguns munch Statements

--- MiniJavaBackend/x86/Codegen.java
+++ MiniJavaBackend/x86/Codegen.java
@@ -1,7 +1,16 @@
 package x86;
 
 import assem.Instr;
+import temp.Temp;
+import tree.CJUMP;
+import tree.EXPSTM;
+import tree.Exp;
+import tree.JUMP;
+import tree.LABEL;
+import tree.MEM;
+import tree.MOVE;
 import tree.Stm;
+import tree.TEMP;
 import util.List;
 
 public class Codegen
@@ -23,8 +32,48 @@ public class Codegen
             last = ilist = new List<Instr>(inst,null);
     }
 
+    // **MUNCH STATEMENT** // 
     private void munchStm (Stm s) {
-    	//TODO: Maximal Munch ou Programacao Dinamica
+    	if (s instanceof MOVE)
+    		munchMove((MOVE) s);    		
+    	else if (s instanceof EXPSTM)
+    		//TODO: munchExpStm((EXPSTM) s);
+    		System.out.println("munchExpStm((EXPSTM) s)");
+    	else if (s instanceof CJUMP)
+    		//TODO: munchCJump((CJUMP) s);
+    		System.out.println("munchCJump((CJUMP) s)");
+    	else if (s instanceof LABEL)
+    		//TODO: munchLabel((LABEL) s);
+    		System.out.println("munchLabel((LABEL) s)");
+    	else if (s instanceof JUMP)
+    		//TODO: munchJump((JUMP) s);
+    		System.out.println("munchJump((JUMP) s)");
+    	else
+    		throw new Error("Unhandled: " + s.getClass());
+    }
+    
+    // **MUNCH MOVE (MOVE)** //
+    private void munchMove (MOVE s){
+    	if (s.dst instanceof MEM)
+    		munchMove((MEM) s.dst, s.src);
+    	else
+    		munchMove((TEMP) s.dst, s.src);
+    }
+    
+    // **MUNCH MOVE (MEM, Exp)** //
+    private void munchMove (MEM dst, Exp src){
+    	Temp val = munchExp(src);
+    	Temp add = munchExp(dst.exp);
+    	
+    	emit (new assem.OPER("mov [`s0] `s1",
+    						  null,
+    						  new List<Temp>(add, new List<Temp>(val,null)))); //add = s0, val = s1
+    	return;
+    }
+    
+ // **MUNCH MOVE (TEMP, Exp)** //
+    private void munchMove (TEMP s, Exp src){
+    	//TODO: implement
     }
     
     /*-------------------------------------------------------------*
